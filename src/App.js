0fa60cab32031d262b0bdc433f83c33d42d2f707
Al-Amin <ialamin.pro@gmail.com>
2020-04-29 12:11:13 +0600

    Dynamic path, set route path parameter

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 import Review from './components/Review/Review';
 import Inventory from './components/Inventory/Inventory';
 import NotFound from './components/NotFound/NotFound';
+import ProductDetail from './components/ProductDetail/ProductDetail';
 
 function App() {
   return (
@@ -29,6 +30,9 @@ function App() {
           </Route>
           <Route exact path="/">
               <Shop></Shop>
+          </Route>
+          <Route path="/product/:productKey">
+            <ProductDetail></ProductDetail>
           </Route>
           <Route path="*">
             <NotFound></NotFound>
