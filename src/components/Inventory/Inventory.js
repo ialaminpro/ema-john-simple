57b822855a5a1ab278294bfc1b1d7b357c9a59df
Al-Amin <ialamin.pro@gmail.com>
2020-04-29 11:24:46 +0600

    Setup Router, Router path, exact path not found path

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            This is Inventory coming soon.
+        </div>
+    );
+};
+
+export default Inventory;
