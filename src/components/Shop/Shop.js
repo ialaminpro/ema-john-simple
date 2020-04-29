f2553f7d746c057dcb833f799c1d3e1e3e9cd6f8
Al-Amin <ialamin.pro@gmail.com>
2020-04-30 01:15:37 +0600

    React Developer Tool, Chrome extension, Refactor add to Cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,11 +9,21 @@ const Shop = () => {
     const [products, setProducts] = useState(first10);
     const [cart, setCart] = useState([]);
     const handleAddProduct = (product) => {
-        const newCart = [...cart, product];
+        const toBeAddedKey = product.key;
+        const sameProduct = cart.find(pd => pd.key === toBeAddedKey);
+        let count = 1;
+        let newCart;
+        if(sameProduct){
+            const count = sameProduct.quantity+1;
+            sameProduct.quantity = count;
+            const others = cart.filter(pd => pd.key !== toBeAddedKey);
+            newCart = [...others, sameProduct];
+        }else{
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }   
         setCart(newCart);
-        const sameProduct = newCart.filter(pd => pd.key===product.key)
-        const count = sameProduct.length
-        addToDatabaseCart(product.key,count);
+        addToDatabaseCart(product.key, count);
     }
     return (
         <div className="shop-container">
