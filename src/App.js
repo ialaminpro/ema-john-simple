1a8625a7a2c2cdd8509f66094ebc4239305ddf7e
Al Amin <ialamin.pro@gmail.com>
2020-05-12 00:42:51 +0600

    Create a simple Context API and user context profiver

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
@@ -15,35 +15,39 @@ import NotFound from './components/NotFound/NotFound';
 import ProductDetail from './components/ProductDetail/ProductDetail';
 import Login from './components/Login/Login';
 
+export const UserContext = createContext();
 function App() {
+  const user = {name: 'Al Amin', email: 'ialamin.pro@gmail.com'}
   return (
     <div>
-      <Header></Header>
-      <Router>
-        <Switch>
-          <Route path="/shop">
-            <Shop></Shop>
-          </Route>
-          <Route path="/review">
-            <Review></Review>
-          </Route>
-          <Route path="/inventory">
-            <Inventory></Inventory>
-          </Route>
-          <Route exact path="/">
+      <UserContext.Provider value={user.name}>
+        <Header></Header>
+        <Router>
+          <Switch>
+            <Route path="/shop">
               <Shop></Shop>
-          </Route>
-          <Route path="/product/:productKey">
-            <ProductDetail></ProductDetail>
-          </Route>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-          <Route path="*">
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-      </Router>
+            </Route>
+            <Route path="/review">
+              <Review></Review>
+            </Route>
+            <Route path="/inventory">
+              <Inventory></Inventory>
+            </Route>
+            <Route exact path="/">
+                <Shop></Shop>
+            </Route>
+            <Route path="/product/:productKey">
+              <ProductDetail></ProductDetail>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+        </Router>
+      </UserContext.Provider>
     </div>
   );
 }
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../../App';
 
 const Cart = (props) => {
     const cart = props.cart;
+    
+    const user = useContext(UserContext);
+    
     // const total = cart.reduce((total, prd) => total + prd.price, 0);
     let total = 0;
     for(let i = 0; i < cart.length; i++){
@@ -39,6 +43,7 @@ const Cart = (props) => {
             {
                 props.children
             }
+            <p>{user}</p>
         </div>
     );
 };
