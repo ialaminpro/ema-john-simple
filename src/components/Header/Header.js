b931cee577f575de988b09e3d4b41de1a7a6cae3
Al Amin <ialamin.pro@gmail.com>
2020-05-12 01:15:00 +0600

    Overview of UseEffect, UseRef, Create a Custom hook

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef, useEffect } from 'react';
 import logo from '../../images/logo.png';
 import './Header.css';
 import { UserContext } from '../../App';
 
+const usePrevious = value => {
+    const prev = useRef();
+    useEffect(()=> {
+        prev.current = value;
+    },[value])
+    return  prev.current;
+}
+
 const Header = () => {
     const user = useContext(UserContext);
+    const [count, setCount] = useState(0);
+    const previous = usePrevious(count);
+    
     return (
         <div className="header">
+            <h1>Count: {count} Previous: {previous}</h1>
+            <button onClick={()=>setCount(count+1)}>+</button>
+            <button onClick={()=>setCount(count-1)}>-</button>
             <img src={logo} alt=""/>
             <nav>
                 <a href="/shop">Shop</a>
